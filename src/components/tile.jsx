df8d3441bc232dd64fe55fa872c391ab80dd3922
bradpickering <bradleepickering@gmail.com>
2022-06-24 21:17:57 -0700

    changed tile size

--- src/components/tile.jsx
+++ src/components/tile.jsx
@@ -1,24 +1,24 @@
 function Tile(props) {
   let tile = (
-    <div className="flex justify-center items-center w-[14vw] h-[14vw] xs:w-[77px] xs:h-[77px] border-2 border-[#3a3a3c]">
+    <div className="flex justify-center items-center w-[14vw] h-[14vw] sm:w-[8vh] sm:h-[8vh] border-2 border-[#3a3a3c]">
       <div className="tile-inner text-2xl text-white">{props.letter}</div>
     </div>
   );
   if (props.color === "green") {
     tile = (
-      <div className="flex justify-center items-center w-[14vw] h-[14vw] xs:w-[77px] xs:h-[77px] border-2 border-black bg-[#538d4e]">
+      <div className="flex justify-center items-center w-[14vw] h-[14vw] sm:w-[8vh] sm:h-[8vh] border-2 border-black bg-[#538d4e]">
         <div className="tile-inner text-2xl text-white">{props.letter}</div>
       </div>
     );
   } else if (props.color === "yellow") {
     tile = (
-      <div className="flex justify-center items-center w-[14vw] h-[14vw] xs:w-[77px] xs:h-[77px] border-2 border-black bg-[#b59f3b]">
+      <div className="flex justify-center items-center w-[14vw] h-[14vw] sm:w-[8vh] sm:h-[8vh] border-2 border-black bg-[#b59f3b]">
         <div className="tile-inner text-2xl text-white">{props.letter}</div>
       </div>
     );
   } else if (props.color === "gray") {
     tile = (
-      <div className="flex justify-center items-center w-[14vw] h-[14vw] xs:w-[77px] xs:h-[77px] border-2 border-black bg-[#403c3c]">
+      <div className="flex justify-center items-center w-[14vw] h-[14vw] sm:w-[8vh] sm:h-[8vh] border-2 border-black bg-[#403c3c]">
         <div className="tile-inner text-2xl text-white">{props.letter}</div>
       </div>
     );
